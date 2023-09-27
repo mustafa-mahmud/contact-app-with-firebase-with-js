15c4b4405a812ba40b9f2c900734bf1375822512
unknown <mithuweb000@gmail.com>
2023-09-28 03:09:50 +0600

    get single data completed

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ import {
   getFirestore,
   addDoc,
   collection,
+  getDocs,
+  getDoc,
+  doc,
+  query,
 } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
 
 const addBtn = document.querySelector('.add-btn');
@@ -11,6 +15,8 @@ const closeBtn = document.querySelector('.close-btn');
 const saveBtn = document.querySelector('.save-btn');
 const form = document.querySelector('form');
 const msg = document.querySelector('.msg');
+const contactList = document.getElementById('contact-list');
+const showContact = document.querySelector('.show-contact');
 
 const db = getFirestore();
 const dbRef = collection(db, 'contact');
@@ -69,17 +75,86 @@ const saveButtonPressed = async (e) => {
 
     initValue();
     showMsg({ cls: 'success', text: 'Contact added successfully' });
+    getContacts();
   } catch (error) {
-    console.log(error);
+    initValue();
+
     showMsg({
       cls: 'danger',
-      text: 'Contact does not added, Pls try later...',
+      text: error.message + '. Pls try later...',
     });
   }
 };
 
-////////////////////////////////////////////////////////////////
+const getContacts = async () => {
+  try {
+    const docRef = collection(db, 'contact');
+    const contacts = await getDocs(docRef);
+    contactList.innerHTML = '';
+
+    contacts.forEach((contact) => {
+      const listItem = `
+			<li class="contact-list-item" data-id="${contact.id}">
+            <div class="media">
+              <div class="two-letters">${contact.data().fName[0]}${
+        contact.data().lName[0]
+      }</div>
+            </div>
+            <div class="content">
+              <div class="title">${contact.data().fName} ${
+        contact.data().lName
+      }</div>
+              <div class="subtitle">${contact.data().email}</div>
+            </div>
+            <div class="action">
+              <button class="edit-user">edit</button>
+              <button class="delete-user">delete</button>
+            </div>
+          </li>
+			`;
+
+      contactList.innerHTML += listItem;
+    });
+  } catch (error) {
+    showMsg({ cls: 'danger', text: `${error.message}. Something wrong...` });
+  }
+};
+
+const singleContact = async (id) => {
+  try {
+    const docRef = doc(db, 'contact', id);
+    const contact = await getDoc(docRef);
+
+    const userInfo = {
+      Name: `${contact.data().fName} ${contact.data().lName}`,
+      Age: contact.data().age,
+      Phone: contact.data().phone,
+      Email: contact.data().email,
+    };
+
+    showContact.innerHTML = ``;
+
+    Object.keys(userInfo).forEach((item) => {
+      showContact.innerHTML += `
+			<div>
+				<h3 class="subject">${item}:</h3>
+				<p class="info">${userInfo[item]}</p>
+			</div>
+			`;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/////////////////////////////////////////////////////////
+getContacts();
 addBtn.addEventListener('click', addButtonPressed);
 closeBtn.addEventListener('click', closeButtonPressed);
 modalOverlay.addEventListener('click', hideModal);
 form.addEventListener('submit', saveButtonPressed);
+contactList.addEventListener('click', (e) => {
+  const id = e.target.closest('.contact-list-item').getAttribute('data-id');
+
+  singleContact(id);
+});
