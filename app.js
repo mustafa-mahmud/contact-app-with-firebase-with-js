6a5a1f1b5c0efb0f1044eca14a5520895a5fba84
unknown <mithuweb000@gmail.com>
2023-09-27 11:59:12 +0600

    addDoc() functionality add

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 'use strict';
+import {
+  getFirestore,
+  addDoc,
+  collection,
+} from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js';
 
 const addBtn = document.querySelector('.add-btn');
 const modalOverlay = document.getElementById('modal-overlay');
 const closeBtn = document.querySelector('.close-btn');
+const saveBtn = document.querySelector('.save-btn');
+const form = document.querySelector('form');
+const msg = document.querySelector('.msg');
+
+const db = getFirestore();
+const dbRef = collection(db, 'contact');
 
 const addButtonPressed = () => {
   modalOverlay.style.display = 'flex';
+};
+
+const initValue = () => {
+  document.querySelector('input[name="fName"]').value = '';
+  document.querySelector('input[name="lName"]').value = '';
+  document.querySelector('input[name="age"]').value = '';
+  document.querySelector('input[name="phone"]').value = '';
+  document.querySelector('input[name="email"]').value = '';
 };
 
 const closeButtonPressed = () => {
@@ -16,7 +35,51 @@ const hideModal = (e) => {
   if (!e.target.closest('.modal')) closeButtonPressed();
 };
 
-/////////////////////////////////////////////////////////////////
+const showMsg = ({ cls, text }) => {
+  msg.classList.add(cls);
+  msg.textContent = text;
+
+  setTimeout(() => {
+    hideMsg();
+  }, 3000);
+};
+
+const hideMsg = () => {
+  closeButtonPressed();
+
+  msg.className = 'msg';
+  msg.textContent = '';
+
+  saveBtn.disabled = false;
+};
+
+const saveButtonPressed = async (e) => {
+  e.preventDefault();
+
+  saveBtn.disabled = true;
+
+  try {
+    await addDoc(dbRef, {
+      fName: document.querySelector('input[name="fName"]').value,
+      lName: document.querySelector('input[name="lName"]').value,
+      age: document.querySelector('input[name="age"]').value,
+      phone: document.querySelector('input[name="phone"]').value,
+      email: document.querySelector('input[name="email"]').value,
+    });
+
+    initValue();
+    showMsg({ cls: 'success', text: 'Contact added successfully' });
+  } catch (error) {
+    console.log(error);
+    showMsg({
+      cls: 'danger',
+      text: 'Contact does not added, Pls try later...',
+    });
+  }
+};
+
+////////////////////////////////////////////////////////////////
 addBtn.addEventListener('click', addButtonPressed);
 closeBtn.addEventListener('click', closeButtonPressed);
 modalOverlay.addEventListener('click', hideModal);
+form.addEventListener('submit', saveButtonPressed);
---
+++ firebase-sdk.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCNtN2ZN_jx2_eITkeHmisgZhmXMTs3nhU',
+  authDomain: 'contact-app-with-vanilla-js.firebaseapp.com',
+  projectId: 'contact-app-with-vanilla-js',
+  storageBucket: 'contact-app-with-vanilla-js.appspot.com',
+  messagingSenderId: '263074169580',
+  appId: '1:263074169580:web:f0f95b4bddab65900d5bc9',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
